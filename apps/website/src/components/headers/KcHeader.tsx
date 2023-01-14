616a29121e4fce81705902e015394d010657afa8
Yasin Torun <yasintorun4680@gmail.com>
2023-01-14 13:25:16 +0300

    Added daisyui and create header

---
+++ apps/website/src/components/headers/KcHeader.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import React, { useEffect } from 'react'
+export type KcHeaderProps = {
+    disableDrawer?: boolean;
+}
+const KcHeader = (props: KcHeaderProps) => {
+
+    const LinkItem = () => (
+        <li>
+            <a>
+        
+            </a>
+        </li>
+    )
+
+
+    return (
+        <header className='lg:w-4/5 m-auto'>
+            <div className="drawer-content flex flex-col">
+                <nav className="navbar drawer-content">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <label tabIndex={0} htmlFor="header-drawer" className="btn btn-ghost md:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><Link href={"/tracks"}>Problemler</Link></li>
+                                <li><Link href={"/contests"}>Yarışmalar</Link></li>
+                                <li><Link href={"/challenges"}>Etkinlikler</Link></li>
+                                <li><Link href={"/projects"}>Projeler</Link></li>
+                            </ul>
+                        </div>
+                        <a className="btn btn-ghost normal-case text-xl">KodChallenge</a>
+                    </div>
+                    <div className="navbar-center hidden md:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            <li><Link href={"/tracks"}>Problemler</Link></li>
+                            <li><Link href={"/contests"}>Yarışmalar</Link></li>
+                            <li><Link href={"/challenges"}>Etkinlikler</Link></li>
+                            <li><Link href={"/projects"}>Projeler</Link></li>
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <a className="btn btn-primary">Giriş Yap</a>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default KcHeader
--- apps/website/src/pages/_document.tsx
+++ apps/website/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap-grid.min.css" integrity="sha512-JQksK36WdRekVrvdxNyV3B0Q1huqbTkIQNbz1dlcFVgNynEMRl0F8OSqOGdVppLUDIvsOejhr/W5L3G/b3J+8w==" crossOrigin="anonymous" referrerPolicy="no-referrer" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
