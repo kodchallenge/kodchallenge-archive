6ce7d9294d36749530a4e7c663fbed448e2946de
Yasin Torun <yasintorun4680@gmail.com>
2023-01-28 12:36:41 +0300

    chore: update editor path

--- apps/website/src/pages/editor/[slug]/index.tsx
+++ apps/website/src/pages/editor/[slug]/index.tsx
@@ -1,12 +1,15 @@
 import { KodEditor } from '@/src/packages/kod-editor'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const index = () => {
+    const router = useRouter()
+    const { slug } = router.query
     return (
         <>
             <Head>
-
+                <title>{slug} - KodChallenge</title>
             </Head>
             <KodEditor />
         </>
